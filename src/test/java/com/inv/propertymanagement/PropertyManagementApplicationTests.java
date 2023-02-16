e01b4776b3c76df8cd02a157d6358724bc4ebb09
Ramiro <mr.ramiro@hotmail.com>
2023-02-16 08:43:59 -0300

    first commit

---
+++ src/test/java/com/inv/propertymanagement/PropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv.propertymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
